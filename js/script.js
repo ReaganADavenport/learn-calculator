3f35f8c31d52e78ecfbdceb2a2e5f08dcf7aa2a6
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-05-08 15:48:44 -0400

    added even listeners and to check to see if the last button push is an operator

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const input = document.getElementById('input'), // input/output button
-    number = document.querySelectorAll('.numbers div'), // number buttons
+    numbers = document.querySelectorAll('.numbers div'), // number buttons
     operator = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
     clear = document.getElementById('clear'); // clear button
@@ -9,9 +9,32 @@ const input = document.getElementById('input'), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
-
+numbers.forEach(function(number){
+    number.addEventListener('click', function(){
+        input.innerHTML += this.innerHTML;
+    });
+});
 // adding click handlers to the operation buttons
-
+operator.forEach(function(operators){
+    operators.addEventListener('click', function(){
+        const lastChar = input.innerHTML[input.innerHTML.length-1];
+        if (lastChar ==='+'||lastChar ==='-'||lastChar==='×'||lastChar==='÷'){
+           let newInput = input.innerHTML.substring(0,input.innerHTML.length-1)+ this.innerHTML;
+           input.innerHTML = newInput; 
+        } else if(input.innerHTML.length == 0){
+            input.innerHTML = "Enter a number, KAREN!";
+        }else{
+            input.innerHTML += this.innerHTML;
+        }
+    });
+});
 // on click of 'equal' button
-
+result.addEventListener('click', function(){
+    input.innerHTML = "Solve your own problems!";
+})
 // clearing the input on press of clear
+clear.addEventListener('click', function(){
+    input.innerHTML = '';
+});
+
+//const someVariable = someString.split(/\+|\-|\×|\÷/g)
