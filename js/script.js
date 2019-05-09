637dbb934a77b1fe646ce83b8201521e24f3705a
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-05-09 13:08:09 -0400

    added maths to it

--- js/script.js
+++ js/script.js
@@ -15,10 +15,10 @@ Array.from(numbers).map(number=> {
         if(resultDisplayed === false){
             input.innerHTML += this.innerHTML;
         } else if (
-            (resultDisplayed === true) && (lastChar ==='+'||lastChar ==='-'|| lastChar ==='*'|| lastChar ==='/') 
+            resultDisplayed === true && lastChar ==='+'||lastChar ==='-'|| lastChar ==='*'|| lastChar ==='/'
         ){
             resultDisplayed = false;
-            input.innerHTML+= this.innerHTML;
+            input.innerHTML += this.innerHTML;
         } else {
             resultDisplayed = false;
             input.innerHTML = '';
@@ -42,11 +42,48 @@ operator.forEach(function(operators){
 });
 // on click of 'equal' button
 result.addEventListener('click', function(){
-    input.innerHTML = "Solve your own problems!";
-})
+    const numsStringArray = input.innerHTML.split(/\+|\-|\*|\//g);
+    const currentString = input.innerHTML;
+    let numsArray =[];
+    numsStringArray.forEach(function(number){
+        numsArray.push(Number(number));
+    });
+    // console.log(numsArray);
+
+    const opsArray = currentString.replace(/[0-9]|\./g, "").split("");
+
+    
+    let multiply = opsArray.indexOf('*');
+    while(multiply != -1){
+        numsArray.splice(multiply, 2, numsArray[multiply]* numsArray[multiply+1]);
+        opsArray.splice(multiply, 1);
+        multiply = opsArray.indexOf('*');
+    }
+
+    let divide = opsArray.indexOf('/');
+    while(divide != -1){
+        numsArray.splice(divide, 2, numsArray[divide]/ numsArray[divide+1]);
+        opsArray.splice(divide, 1);
+        divide = opsArray.indexOf('/');
+    }
+
+    let add = opsArray.indexOf('+');
+    while(add != -1){
+        numsArray.splice(add, 2, numsArray[add]+ numsArray[add+1]);
+        opsArray.splice(add, 1);
+        add = opsArray.indexOf('+');    
+    }
+
+    let subtract = opsArray.indexOf('-');
+    while(subtract != -1){
+        numsArray.splice(subtract, 2, numsArray[subtract]- numsArray[subtract+1]);
+        opsArray.splice(subtract, 1);
+        subtract = opsArray.indexOf('-');
+    }
+    resultDisplayed = true;
+    input.innerHTML = numsArray;
+});
 // clearing the input on press of clear
 clear.addEventListener('click', function(){
-    input.innerHTML = '';
+   input.innerHTML = "";
 });
-
-//const someVariable = someString.split(/\+|\-|\×|\÷/g)
